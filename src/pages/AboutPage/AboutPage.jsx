110bedf3897e7c8cd578b16975ea0674a1266684
Alex <alexstocking@hotmail.co.uk>
2024-04-11 13:56:40 +0100

    AboutUs Picture Fix

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../../App.css';
+import Amanda from '../../assets/Amanda.jpg'
 
 
 export default function About() {
@@ -8,7 +9,7 @@ export default function About() {
     <Container className='font'>
       <Row>
         <Col xs={12} md={6}>
-          <img src="https://media.discordapp.net/attachments/759493619362168893/1212887000961781851/IMG_0687.jpg?ex=65f3780b&is=65e1030b&hm=89a30fc492ab43f866e8166fb681fe172f6a8e3f81472c61a264faa4c0f306f8&=&format=webp&width=503&height=671" alt="Sage and Whistle" className="img-fluid mb-4" />
+          <img src={Amanda} alt="Sage and Whistle" className="img-fluid mb-4" />
         </Col>
         <Col xs={12} md={6}>
           <h1>About Sage and Whistle</h1>
